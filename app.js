9754e342ba076cc36ba6d9a27217bb46c9947b7c
HETIC <rami.cakir01@gmail.com>
2020-03-21 14:27:01 +0100

    step 13

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const axios = require("axios");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
 
+const title = "Qui prend quoi ?";
+
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-  res.render("index", { title: "Qui prend quoi ?" });
+  res.render("index", { title: title });
 });
 
 app.post("/party", function(req, res) {
@@ -17,6 +19,10 @@ app.post("/party", function(req, res) {
     .post(`${process.env.API_URL}/party`, req.body)
     .then(({ data }) => console.log(data))
     .catch(err => console.error(err));
+});
+
+app.get("/party/:id", function(req, res) {
+  res.render("party", { title: title, subtitle: "Page événement" });
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
