cc141778306c53cf6c48478703643cd33421cb10
HETIC <rami.cakir01@gmail.com>
2020-03-21 14:17:51 +0100

    step 11

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
 
 app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.render("index", { title: "Qui prend quoi ?" });
