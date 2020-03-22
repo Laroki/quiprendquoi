d13a66712c93768b49ee6ca2b5000b4532646c7e
HETIC <rami.cakir01@gmail.com>
2020-03-22 15:06:59 +0100

    step 26

--- app.js
+++ app.js
@@ -8,35 +8,33 @@ const port = process.env.PORT;
 const title = "Qui prend quoi ?";
 
 app.set("view engine", "pug");
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 // Index
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("index", {
     title: title
   });
 });
 
 // Create party
-app.post("/party", function (req, res) {
+app.post("/party", function(req, res) {
   axios
     .post(`${process.env.API_URL}/party`, req.body)
-    .then(({
-      data
-    }) => res.redirect(`/party/${data._id}`))
+    .then(({ data }) => res.redirect(`/party/${data._id}`))
     .catch(err => res.send(err));
 });
 
 // Get party by id
-app.get("/party/:id", function (req, res) {
+app.get("/party/:id", function(req, res) {
   axios
     .get(`${process.env.API_URL}/party/${req.params.id}`)
-    .then(({
-        data
-      }) =>
+    .then(({ data }) =>
       res.render("party", {
         party: data,
         title: data.name,
@@ -47,7 +45,7 @@ app.get("/party/:id", function (req, res) {
 });
 
 // Create item
-app.post("/item/:id", function (req, res) {
+app.post("/item/:id", function(req, res) {
   axios
     .post(`${process.env.API_URL}/party/${req.params.id}/items`, req.body)
     .then(() => res.redirect(`/party/${req.params.id}`))
@@ -55,7 +53,7 @@ app.post("/item/:id", function (req, res) {
 });
 
 // Delete Item, this route doesn't seems to work
-app.post("/party/:partyId/items/:itemId", function (req, res) {
+app.post("/party/:partyId/items/:itemId", function(req, res) {
   axios
     .delete(
       `${process.env.API_URL}/party/${req.params.partyId}/items/${req.params.itemId}`
