78583038729510f565a4117668907f6d34b52341
HETIC <rami.cakir01@gmail.com>
2020-03-22 15:38:33 +0100

    step 33

--- sw.js
+++ sw.js
@@ -4,3 +4,10 @@ console.log(`Hello ${name}`);
 addEventListener("install", event => {
   console.log("Hello from the service worker");
 });
+
+addEventListener("fetch", event => {
+  console.log(event);
+  if (event.request.headers.get("Accept").includes("text/html")) {
+    event.respondWith(fetch(event.request));
+  }
+});
